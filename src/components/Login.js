42685f602312da6ca87b680a9705c3ccbb312643
devjaveriasohail <javeriasohail996@gmail.com>
2022-12-17 22:11:31 +0500

    git commit -m "login"

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ function Login() {
     const [user, setUser] = useState('');
     const [pasword, setPasword] = useState('');
     const [loginData, setloginData] = useState();
-    const [sucess,setSucess]=useState(false);
+    const [sucess, setSucess] = useState(false);
 
     useEffect(() => {
         userRef.current.focus();
@@ -29,12 +29,12 @@ function Login() {
         compareData();
         fetchData();
     }
-    
+
     const fetchData = async () => {
-       await fetch(`https://www.melivecode.com/api/users?search=${user}`)
+        await fetch(`https://www.melivecode.com/api/users?search=${user}`)
             .then((res) => res.json())
             .then((data) => setloginData(data));
-            console.log("fetch data", loginData)
+        console.log("fetch data", loginData)
     };
 
     useEffect(() => {
@@ -45,50 +45,54 @@ function Login() {
 
     return (
         <>
-        {sucess ? (
-             <section>
-                 <Header loginData={loginData}/> 
-                <h1 className='text-center m-4 p-4'>You have Logged in!</h1>
-             </section>
-        ):(
-            <section className="Section">
-            <h1 className='display-4 text-center'><strong><em> Log In</em> </strong></h1>
-            <div className="container">
-
-                <form onSubmit={submitButon}>
-                    <div className="mb-3">
-                        <label htmlFor="Username" className="form-label">Username:</label>
-                        <input className="form-control"
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            onChange={(ele) => setUser(ele.target.value)}
-                            value={user}
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="Password" className="form-label">Password:</label>
-                        <input className="form-control"
-                            type="password"
-                            id="password"
-                            onChange={(ele) => setPasword(ele.target.value)}
-                            value={pasword}
-                            required
-                            aria-describedby="passwordHelpBlock"
-                        />
-                        <div id="passwordHelpBlock" className="form-text">
-                            Your password must be 8-20 characters long, contain letters and numbers.
+            {sucess ? (
+                <section>
+                    <Header loginData={loginData} />
+                    <h1 className='text-center m-4 p-4'>You have Logged in!</h1>
+                </section>
+            ) : (
+                <section className="Section">
+                <div class="card border-dark">
+                    <h1 className='display-4 text-center card-header'><strong><em> LOGIN</em> </strong></h1>
+                    <div class="card-body">
+                        <div className="container">
+                            <form onSubmit={submitButon}>
+                                <div className="mb-3">
+                                    <label htmlFor="Username" className="form-label">Username:</label>
+                                    <input className="form-control"
+                                        type="text"
+                                        id="username"
+                                        ref={userRef}
+                                        onChange={(ele) => setUser(ele.target.value)}
+                                        value={user}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="Password" className="form-label">Password:</label>
+                                    <input className="form-control"
+                                        type="password"
+                                        id="password"
+                                        onChange={(ele) => setPasword(ele.target.value)}
+                                        value={pasword}
+                                        required
+                                        aria-describedby="passwordHelpBlock"
+                                    />
+                                    <div id="passwordHelpBlock" className="form-text">
+                                        Your password must be 8-20 characters long, contain letters and numbers.
+                                    </div>
+                                </div>
+                                <div className="d-grid gap-2 d-md-flex justify-content-md-end mr-4">
+                                <button className='btn btn-dark btn-lg '>Login</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                    <button className='btn btn-secondary'>Login</button>
+                </div>
+             </section>
+            )}
 
-                </form>
-            </div>
-        </section>
-        )}
 
-           
         </>
     )
 }
